92ed8fcafff44f864ecfc633a290e184e2b01f11
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-03-26 13:57:42 +0200

    Модул 06, Видео 06 - Форматиране на конзолно приложение

--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -4,9 +4,25 @@ namespace UserInput
 {
 	public class UInput
 	{
+		About.Me about = new About.Me ();
+		Colors.ForCLI _c = new Colors.ForCLI ();
 		public UInput ()
 		{
 		}
+
+
+
+
+		public void sayHello ()
+		{
+			_c.Default ();
+			Console.WriteLine ("Добре дошли в " + about.shortName + "\n" + about.version + "\n");
+		}
+
+
+
+
+
 
 		public void getUserCommands ()
 		{
@@ -14,11 +30,15 @@ namespace UserInput
 
 			do {
 				//Взимане на команда
-				Console.Write (" $ ");
-				_command = Console.ReadLine ();
+				_c.Prompt();        Console.Write (" $ ");
+				_c.Command ();      _command = Console.ReadLine ();
 
 				//Проверка за налични команди
-				if (_command.ToLower ().Contains ("команда1") )               Console.WriteLine ("Стартирана е команда 1 \n");
+				if (_command.ToLower ().Contains ("команда1") )              
+				{
+					_c.Default ();     Console.Write ("Стартирана е ");
+					_c.Result ();      Console.WriteLine ("Команда 1 \n");
+				}
 				if (_command.ToLower ().Contains ("команда2") )               Console.WriteLine ("Стартирана е команда 2 \n");
 				if (_command.ToLower ().Contains ("команда3") )               Console.WriteLine ("Стартирана е команда 3 \n");
 
