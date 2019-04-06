2252a2871b988ef1ff0b14362b19d0069002c93f
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-04-06 13:02:16 +0300

    Модул 06, Видео 08 - Форматиране на първата формула

--- MP01/Calculations/Formula01.cs
+++ MP01/Calculations/Formula01.cs
@@ -4,6 +4,8 @@ namespace Calculations
 {
 	public class Formula01
 	{
+		//Библиотеки
+		private Colors.ForCLI _c = new Colors.ForCLI ();
 		public Formula01 () //Формула за обикновена строителна яма
 		{
 		}
@@ -18,6 +20,7 @@ namespace Calculations
 
 				if (param.Length > 1 && _userInput.Contains (" -п")) {
 					//Помощ за командата
+					help ();
 				}
 
 				if (param.Length > 1 && !_userInput.Contains (" -п")) {
@@ -25,10 +28,11 @@ namespace Calculations
 					double _result = 0;
 					if ( runCalculations (param, out _result ) )
 					{
-						Console.Write ("Обема на строителната яма е: ");
-						Console.WriteLine (_result.ToString ("N2"));
+						_c.Default ();  Console.Write ("Обема на строителната яма е: ");
+						_c.Result ();   Console.WriteLine (_result.ToString ("N2"));
+						_c.Default ();  Console.WriteLine (" м3\n ");
 					}else{
-						Console.WriteLine ("Има грешно въведени елементи. Можете да проверите синтаксиса с параметъра '-п' ");
+						
 					}
 				}
 
@@ -42,22 +46,49 @@ namespace Calculations
 			try {
 				double _a1 = 0, _b1 = 0, _a2 = 0, _b2 = 0, _h = 0;
 
-				Double.TryParse (_param[1], out _a1);
-				Double.TryParse (_param[2], out _b1);
-				Double.TryParse (_param[3], out _a2);
-				Double.TryParse (_param[4], out _b1);
-				Double.TryParse (_param[5], out _h);
+				Double.TryParse (_param [1], out _a1);
+				Double.TryParse (_param [2], out _b1);
+				Double.TryParse (_param [3], out _a2);
+				Double.TryParse (_param [4], out _b1);
+				Double.TryParse (_param [5], out _h);
 
 				double F1 = _a1 * _b1, F2 = _a2 * _b2;
 				_result = _h * (F1 + F2) / 2;
 				return true;
 
-			}catch{
+			} catch {
 			}
-				_result = 0;
-				return false;
+			_result = 0;
+			return false;
+		}
+
 		//Помощ за команндата
+			private void help ()
+			{
+			_c.Result ();     Console.Write ("[яма]");
+			_c.Default ();    Console.WriteLine (" - команда за пресмятане на строителна яма");
+
+
+			_c.Command ();     Console.Write ("параметри: ");
+			_c.Default ();    Console.WriteLine ("a1 b1 a2 b2 h\n");
+
+
+			_c.Command ();     Console.Write ("a1 и b1 ");
+			_c.Default ();    Console.WriteLine (" - ширина и дължина на горната страна на изкопа ");
+
+
+			_c.Command ();     Console.Write ("a2 и b2");
+			_c.Default ();    Console.WriteLine (" - ширина и дължина на долната страна на изкопа");
+
+
+			_c.Command ();     Console.Write ("h ");
+			_c.Default ();    Console.WriteLine (" - височина на изкопа");
+			}
 	}
 }
-	}
+	
+
+
+
+
 
